f5ea925ed97b3c435fe2a0cfb2c67a0472a2bd4a
Micheal Rapheal Abah <38321155+Smartmikey@users.noreply.github.com>
2018-04-12 16:48:06 +0100

    #stage1 work
    My #stage1 html and css project

---
+++ figma.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My figma stage 1</title>
+		<link rel="stylesheet" href="figma.css">
+	</head>
+	<body>
+			<div id="wrapper">
+				<div><h2 id="head">www.smartmikey.com</h2></div>
+				<ul id="nav">
+					<li><span>Home</span></li>
+					<li>About</li>
+					<li>Contact</li>
+					<li>Share</li>
+				</ul> <br>
+				<div id="content">
+					<div id="sidebar">
+						<ul>
+							<li class="our">OUR PRODUCTS:</li>
+							<li># Web design</li>
+							<li># Graphics design</li>
+							<li># Software design</li>
+							<li># Programming</li>
+							<li># Web development</li>
+						</ul>
+					</div>
+					<div id="main">
+						<div id="head">Awesome design:</div>
+						<div>Awesome designis a company envision by Abah Micheal,<br> which is Aimed 
+						at improving the value of designs in all aspect through its trained workers.</div>
+					</div>
+
+					<div id="time"><?php echo $today = date("F j, Y, g:i a"); ; ?></div>
+				</div><br>
+				<div id="foot">copyright&copy  this is the property of Smartmikey. all right reserved</div>
+
+			</div>
+	</body>
+</html>
